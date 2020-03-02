0237c564501a16a7ec9dc2952ad7869c815547e2
Qasim Aslam <muhammadqasimaslam3@gmail.com>
2020-03-02 15:45:52 +0500

    Admin Panel - Add Category

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,4 +1,33 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+
+    include "config.php"; // database configuration
+    
+    if(isset($_POST['save'])){      // when clicks on save button 
+        $category = $_POST['cat'];
+
+    /* query for check input value exists in category table or not*/
+    $sql = "SELECT category_name FROM categories WHERE category_name='{$category}'";
+    $result = mysqli_query($conn, $sql);
+    
+    if(mysqli_num_rows($result) > 0){
+         // if input value exists
+         echo "<p style = 'color:red;text-align:center;margin: 10px 0';> Category already exists.</p>";
+    }else{
+        // if category does not exist insert into database
+       $sql = "INSERT INTO categories (category_name) 
+                VALUES ('{$category}')";
+
+        if(mysqli_query($conn, $sql)){
+        header("Location: {$hostname}/admin/category.php");
+        }else{
+            echo "<p style = 'color:red;text-align:center;margin: 10px 0';>Query Failed.</p>";
+        }
+    }
+}
+    mysqli_close($conn);
+    
+
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -7,7 +36,7 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
-                  <form action="" method="POST" autocomplete="off">
+                  <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" autocomplete="off">
                       <div class="form-group">
                           <label>Category Name</label>
                           <input type="text" name="cat" class="form-control" placeholder="Category Name" required>

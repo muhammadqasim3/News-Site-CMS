5e2460a06f78b91a31234532bfca941ba6fc73b5
Qasim Aslam <muhammadqasimaslam3@gmail.com>
2020-03-02 16:44:15 +0500

    Admin Panel - Category Listing

--- admin/category.php
+++ admin/category.php
@@ -1,4 +1,8 @@
-<?php include "header.php"; ?>
+<?php include "header.php"; 
+
+    include "config.php"; // database configuration 
+?>
+
 <div id="admin-content">
     <div class="container">
         <div class="row">
@@ -9,59 +13,47 @@
                 <a class="add-new" href="add-category.php">add category</a>
             </div>
             <div class="col-md-12">
-                <table class="content-table">
-                    <thead>
-                        <th>S.No.</th>
-                        <th>Category Name</th>
-                        <th>No. of Posts</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class='id'>1</td>
-                            <td>Html</td>
-                            <td>5</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>2</td>
-                            <td>Css</td>
-                            <td>15</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>3</td>
-                            <td>Java</td>
-                            <td>8</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>4</td>
-                            <td>Php</td>
-                            <td>11</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>5</td>
-                            <td>Python</td>
-                            <td>13</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                        <tr>
-                            <td class='id'>6</td>
-                            <td>Scss</td>
-                            <td>3</td>
-                            <td class='edit'><a href='update-category.php'><i class='fa fa-edit'></i></a></td>
-                            <td class='delete'><a href='delete-category.php'><i class='fa fa-trash-o'></i></a></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <?php
+                    // Pagination Offset
+                    $limit = 3;
+                    if(isset($_GET["page"])){
+                        $page = $_GET["page"];
+                    }else{
+                        $page = 1;
+                    }
+                    $offset = ($page - 1) * $limit;
+                    /* select query with offset and limit */
+                    $sql = "SELECT * FROM  categories ORDER BY category_id DESC Limit $offset,$limit";
+                    $result = mysqli_query($conn, $sql);
+                    
+                    if(mysqli_num_rows($result) > 0){
+                       echo '<table class="content-table">
+                            <thead>
+                                <th>S.No.</th>
+                                <th>Category Name</th>
+                                <th>No. of Posts</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </thead>
+                            <tbody>';
+                            $serial = $offset + 1;
+                            while($row = mysqli_fetch_assoc($result)){
+                           echo     "<tr>
+                                    <td class='id'>{$serial}</td>
+                                    <td>{$row["category_name"]}</td>
+                                    <td>{$row["post"]}</td>
+                                    <td class='edit'><a href='update-category.php?id={$row['category_id']}' ><i class='fa fa-edit'></i></a></td>
+                                    <td class='delete'><a href='delete-category.php?id={$row['category_id']}'><i class='fa fa-trash-o'></i></a></td>
+                                </tr>";
+                                $serial++;
+                            }
+                           echo '</tbody></table>';
+                    }else{
+                        echo "<h3>No Results Found.</h3>";
+                    }
+                    
+                ?>
+
                 <ul class='pagination admin-pagination'>
                     <li class="active"><a>1</a></li>
                     <li><a>2</a></li>
